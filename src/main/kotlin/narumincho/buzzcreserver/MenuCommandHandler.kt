d116c4842ed9d6aef8ed72bef2122bdd349c28c2
narumincho <16481886+narumincho@users.noreply.github.com>
2022-07-24 08:16:42 +0900

    ポータルのバグが解消された分の変更

--- src/main/kotlin/narumincho/buzzcreserver/MenuCommandHandler.kt
+++ src/main/kotlin/narumincho/buzzcreserver/MenuCommandHandler.kt
@@ -8,6 +8,7 @@ import org.bukkit.command.CommandSender
 class MenuCommandHandler : CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
         Bukkit.getLogger().info("m が呼ばれた")
+        sender.sendMessage(sender.name + "さん. /m の メニュー機能は開発中なんだ")
         executeCommand("list", arrayOf())
         return true
     }
